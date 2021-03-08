0dd91f63ad75a87fd34b44d0a578daa06ef827de
Fernando Grandez <f.grandez310@gmail.com>
2021-03-07 21:33:01 -0500

    Testing, testing, 1...2...3...

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         File dataFile = new File("Users/user/Documents/data.txt");
         Scanner scan = new Scanner(dataFile);
 
-        //beau bailey flacuchento DM ;D
+        //beau bailey flacuchento DM :D
 
         DynamicQueue[] queues = new DynamicQueue[4];
 
