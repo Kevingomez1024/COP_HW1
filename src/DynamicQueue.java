484ed0fcff4734749211988cc8c624d2b2460ffe
Kevin Gomez <khgomez2002@eagle.fgcu.edu>
2021-03-08 16:18:32 -0500

    Added methods to DynamicQueue

--- src/DynamicQueue.java
+++ src/DynamicQueue.java
@@ -1,6 +1,7 @@
 class DynamicQueue{
     private DynamicNode front, rear;
-    int QUEUESIZE = 4;
+    final int QUEUESIZE = 4;
+    int currentSize = 0;
 
     public DynamicQueue(){
         front = rear = null;
@@ -10,8 +11,62 @@ class DynamicQueue{
         return (front == null);
     }
 
-    public void addQueue(char key, int index) {
+    public DynamicNode insertIntoQueue(Object x){
+        if(this.empty()){ // insert and return node if no nodes are in queue
+            front = rear = new DynamicNode(x,null);
+            currentSize = 1;
+            return rear;
+        }
+        DynamicNode t = null,q = null;
+        for(DynamicNode p = front; p != null; p = p.getNext()){ // checks and inserts duplicates
+            q = p.getNext();
+            if(x.equals(front.getInfo())){ // if duplicate is in first node, seat rear next to front. Set front next to front;
+                rear.setNext(front);
+                front = p.getNext();
+                rear = rear.getNext();
+                rear.setNext(null);
+                return rear;
+            }
+            else if(x.equals(rear.getInfo())){
+                return rear;
+            }
+            if(x.equals(p.getInfo())){
+                rear.setNext(p);
+                t.setNext(q);
+                rear = rear.getNext();
+                rear.setNext(null);
+                return rear;
+            }
+            t = p;
+        }
+
+        if(currentSize == 4){
+            front = front.getNext();
+            rear.setNext(new DynamicNode(x,null));
+            rear = rear.getNext();
+            return rear;
+        }
+        else{
+            rear.setNext(new DynamicNode(x,null));
+            rear = rear.getNext();
+            currentSize++;
+            return rear;
+        }
+
 
     }
+
+    public String printQueue(){
+        String queue = "";
+        DynamicNode p = front;
+        while(p != null){
+            queue = queue + p.getInfo() + "->";
+            p = p.getNext();
+        }
+        return queue;
+    }
+
+
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,27 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        File dataFile = new File("/Users/user/IdeaProjects/COP_HW1/src/data.txt");
-        Scanner scan = new Scanner(dataFile);
+        //File dataFile = new File("Users/user/Documents/data.txt");
+        //Scanner scan = new Scanner(dataFile);
 
-
-        while(scan.hasNextLine()) { //loop to read the entire file
-            String[] line = scan.nextLine().split(" "); //array to hold the key and index provided in each line
-            char key = line[0].charAt(0);
-            int index = Integer.parseInt(line[1]);
-
-        }
-
-
-
-
-
+        //beau bailey flacuchento DM :D
 
         DynamicQueue[] queues = new DynamicQueue[4];
 
 
+
+
+
+        DynamicQueue queue = new DynamicQueue();
+        queue.insertIntoQueue("a");
+        queue.insertIntoQueue("b");
+        queue.insertIntoQueue("c");
+        queue.insertIntoQueue("b");
+
+        System.out.println(queue.printQueue());
 
 
     }
