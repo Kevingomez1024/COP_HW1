0772325457f9576bd6b69614f4c8cd63e8ea4024
Fernando Grandez <f.grandez310@gmail.com>
2021-03-07 21:23:53 -0500

    Testing, testing, 1...2...3...

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        File dataFile = new File("Users/user/Documents/data.txt");
+        Scanner scan = new Scanner(dataFile);
+
+        //beau bailey flacuchento DM ;D
+
         DynamicQueue[] queues = new DynamicQueue[4];
 
 
