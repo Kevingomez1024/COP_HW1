c2b5ab4ef83306ed42f8f0f7860b0ea52891805e
Fernando Grandez <f.grandez310@gmail.com>
2021-03-07 22:38:00 -0500

    Added variables to hold the key and index when read from the file do you see this?

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,26 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         File dataFile = new File("/Users/user/IdeaProjects/COP_HW1/src/data.txt");
         Scanner scan = new Scanner(dataFile);
-        System.out.println(scan.nextLine());
+
+
+        while(scan.hasNextLine()) { //loop to read the entire file
+            String[] line = new String[2]; //array to hold the key and index provided in each line
+            line = scan.nextLine().split(" "); //obtaining the key and index by splitting the line wherever there is a space
+            char key = line[0].charAt(0); //setting the key equal
+            int index = Integer.parseInt(line[1]); //setting the index
+            System.out.println(key + " " + index);
+
+        }
+
+
+
 
 
 
