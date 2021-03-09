ce097b6501d5b7d79f25e4edfa04d584e5acc543
Kevin Gomez <khgomez2002@eagle.fgcu.edu>
2021-03-09 18:31:14 -0500

    Added methods to DynamicQueue

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        //File dataFile = new File("Users/user/Documents/data.txt");
-        //Scanner scan = new Scanner(dataFile);
+        File dataFile = new File("/Users/user/IdeaProjects/COP_HW1/src/data.txt");
+        Scanner scan = new Scanner(dataFile);
 
-        //beau bailey flacuchento DM :D
+        while(scan.hasNextLine()) { //loop to read the entire file
+            String[] line = scan.nextLine().split(" "); //array to hold the key and index provided in each line
+            char key = line[0].charAt(0);
+            int index = Integer.parseInt(line[1]);
+        }
 
         DynamicQueue[] queues = new DynamicQueue[4];
         queues[0] = new DynamicQueue(4);
